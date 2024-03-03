36754530aab8a6390c2c3aca2de5593865529d9e
tkeatkaew <thanit@rmutl.ac.th>
2024-03-03 20:22:54 +0700

    users edited

--- Step1/server/controllers/users.js
+++ Step1/server/controllers/users.js
@@ -1,16 +1,35 @@
+const fetch = require("cross-fetch");
 const { response } = require("express");
 const axios = require('axios');
 
 const users = async (req, res = response) => {
 
   try {
+    /*
+        axios.get('https://randomuser.me/api/?page=1&results=10')
+            .then(response => {
+                res.send(response.data);
+                res.json(response.data);
+    
+             });
+    */
+    const resp = await fetch(
+      `https://randomuser.me/api/?page=1&results=10`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
 
-    axios.get('https://randomuser.me/api/?page=1&results=10')
-        .then(response => {
-            //res.send(response.data);
-            res.json(response.data);
+    if (resp.status >= 400) {
+      throw new Error("Bad response from server");
+    }
 
-         });
+    const { data = [] } = await resp.json();
+
+    res.json(data);
 
   } catch (err) {
     console.error(err);
--- Step1/server/routes/users.js
+++ Step1/server/routes/users.js
@@ -1,4 +1,4 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const router = Router();
 
 const { users } = require('../controllers/users');
